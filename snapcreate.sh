4c9a6faf68d44b1ba4fa364b2673dda8902324b5
Sakis Karagiannis <sakiskar90@gmail.com>
2017-05-01 22:35:20 +0100

    ADD: Kargo for kubernetes deployment

--- snapcreate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..3}
-do
-	vagrant snapshot save "cluster-node-$i" "$1-$i"
-done
--- snaprestore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..3}
-do
-	vagrant snapshot restore "cluster-node-$i" "$1-$i"
-done
