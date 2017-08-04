3bb7936c7751c554990840fba73f14f61489dd7d
Sakis Karagiannis <sakiskar90@gmail.com>
2017-08-04 12:46:48 +0100

    CHG: slurmd liveness to filter by slurm's --dead flag

--- slurm-dockerfiles/slurmd/liveness.sh
+++ slurm-dockerfiles/slurmd/liveness.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 hostname="$(hostname -f)"
-status="$(sinfo -h -n $hostname | awk '{ print $5 }')"
-if [ "$status" = "idle" ] || [ "$status" = "alloc"]
-then
-  exit 0
-else
-  exit 1
-fi
+status="$(sinfo -h -n $hostname --dead | awk '{ print $4 }')"
 
+exit $status
