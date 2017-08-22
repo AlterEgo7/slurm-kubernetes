c82cc6d68aafe1b9ebd10a70614d522a37146cbd
Sakis Karagiannis <sakiskar90@gmail.com>
2017-08-22 19:39:33 +0100

    FIX: Scripts for removing drained nodes

--- slurm-dockerfiles/slurmctl/config-reload.sh
+++ slurm-dockerfiles/slurmctl/config-reload.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-supervisorctl restart slurmctld
+read WORKER_HOST
+supervisorctl restart slurmctld > /dev/null
 scontrol reconfigure
+
+status="$(sinfo -n $WORKER_HOST -h | awk '{ print $5 }')"
+
+if ["$status" = "drain" ]
+then
+  scontrol update nodename=$WORKER_HOST state=resume
+fi
+
 echo "$(date +'%F %T'): Slurm configuration updated"
--- slurm-dockerfiles/slurmd/preStop.sh
+++ slurm-dockerfiles/slurmd/preStop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "$(hostname -f)" | nc slurm-master.slurm 22345
+HOSTNAME="$(hostname -f)"
+
+# Drain host
+echo $HOSTNAME | nc slurm-master.slurm 22345
 
 timeout 30 bash -c 'until [ "$(sinfo -h -n $(hostname -f) | awk '\''{ print $5 }'\'')" = "drain" ]; do sleep 5; done'
 
 java -jar /hooks.jar preStop
+
+# Issue reconfigure to remove host
+echo $HOSTNAME | nc slurm-master.slurm 22346
